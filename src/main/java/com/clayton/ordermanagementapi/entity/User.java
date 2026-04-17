cff20b5b2a25d6d0595bae5e46411e20def380b8
Clayton Santos <claytonsantos20011@gmail.com>
2026-04-17 07:03:21 -0300

    feat(security): integrate UserDetails and add security dependencies

--- src/main/java/com/clayton/ordermanagementapi/entity/User.java
+++ src/main/java/com/clayton/ordermanagementapi/entity/User.java
@@ -1,15 +1,20 @@
 package com.clayton.ordermanagementapi.entity;
-
 import com.clayton.ordermanagementapi.enums.Role;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Setter
 @Getter
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,4 +33,33 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
