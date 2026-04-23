ee46410c10cf592390a295eab4c8a9e649cf10fc
Clayton Santos <claytonsantos20011@gmail.com>
2026-04-23 05:30:35 -0300

    feat(jwt): generate token on successful login

--- src/main/java/com/clayton/ordermanagementapi/controller/AuthController.java
+++ src/main/java/com/clayton/ordermanagementapi/controller/AuthController.java
@@ -2,6 +2,9 @@ package com.clayton.ordermanagementapi.controller;
 
 import com.clayton.ordermanagementapi.dto.LoginRequest;
 import com.clayton.ordermanagementapi.dto.LoginResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/login")
     public LoginResponse login(@RequestBody LoginRequest request){
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
         return new LoginResponse("Login sucessful");
     }
-
 }
