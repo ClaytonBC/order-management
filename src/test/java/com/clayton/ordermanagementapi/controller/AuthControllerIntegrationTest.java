b9dbc8de993945f7a72200f8e37ffe47aa081ce6
Clayton Santos <claytonsantos20011@gmail.com>
2026-04-30 01:05:07 -0300

    test(integration): add AuthController integration test for login and JWT

---
+++ src/test/java/com/clayton/ordermanagementapi/controller/AuthControllerIntegrationTest.java
@@ -0,0 +1,51 @@
+package com.clayton.ordermanagementapi.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles("test")
+class AuthControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldLoginAndReturnToken() throws Exception {
+
+        String registerJson = """
+        {
+            "name": "Clayton",
+            "email": "test@email.com",
+            "password": "123456"
+        }
+        """;
+
+        mockMvc.perform(post("/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(registerJson))
+                .andExpect(status().isOk());
+
+        String loginJson = """
+        {
+            "email": "test@email.com",
+            "password": "123456"
+        }
+        """;
+
+        mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(loginJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.token").exists());
+    }
+}
